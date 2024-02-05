d111ca6b58e681dc782ea5c689a7ed2c8ab115da
gtcore902 <gaetan.tremois@gmail.com>
2024-02-05 09:23:43 +0100

    style: implement sass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo_kasa.svg';
+import '../styles/Header.sass';
 
 const Header = () => {
   return (
-    <div>
+    <div className="navbar">
       <img src={logo} alt="Logo Kasa" />
       <Link to="/">Accueil</Link>
       <Link to="about">A Propos</Link>
