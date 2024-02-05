af3d259db63eb627df25fff1d8dc80c1327ece25
gtcore902 <gaetan.tremois@gmail.com>
2024-02-05 21:03:30 +0100

    perf: separate router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState, createContext } from 'react';
 import * as datas from './datas/logements.json';
-
-import Header from './components/Header';
-import Home from './pages/Home';
-import Hebergment from './pages/Hebergment';
-import About from './pages/About';
-import Error from './pages/Error';
+import Router from './components/Router';
 
 // Create context and export
 export const ThemeContext = createContext();
@@ -14,7 +8,6 @@ export const ThemeContext = createContext();
 function App() {
   const [hebergments, setHebergments] = useState([]);
   // Set state with datas from logements.json
-  console.log(hebergments);
   useEffect(() => {
     setHebergments(Array.from(datas));
     // console.log(hebergments);
@@ -22,15 +15,7 @@ function App() {
 
   return (
     <ThemeContext.Provider value={hebergments}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hebergment/:hebergmentId" element={<Hebergment />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </ThemeContext.Provider>
   );
 }
---
+++ src/components/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from '../pages/Home';
+import Hebergment from '../pages/Hebergment';
+import About from '../pages/About';
+import Error from '../pages/Error';
+
+const Router = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hebergment/:hebergmentId" element={<Hebergment />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Router;
