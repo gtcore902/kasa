42e914a3581c2e9aa9eeafffd053f16a9266e3ae
gtcore902 <gaetan.tremois@gmail.com>
2024-02-06 16:45:04 +0100

    feat: implemente img and header in hebergment page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,28 +12,30 @@ const About = () => {
         backgroundImage={mountainBackground}
         altAttribute="Illustration d'une chaîne de montagne"
       />
-      <Collapse
-        title="Fiabilité"
-        content="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+      <div className="collapse-container">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. 
         Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-      />
-      <Collapse
-        title="Respect"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
         Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Service"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+        />
+        <Collapse
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
         Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Sécurité"
-        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
         chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi à 
         l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
         Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      />
+        />
+      </div>
       <Footer />
     </div>
   );
--- src/pages/Hebergment.jsx
+++ src/pages/Hebergment.jsx
@@ -2,6 +2,7 @@ import { ThemeContext } from '../App';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import '../styles/Hebergment.sass';
 
 const Hebergment = () => {
   const navigate = useNavigate();
@@ -18,8 +19,40 @@ const Hebergment = () => {
 
   if (targetedHebergment.length === 1) {
     return (
-      <div>
-        Hébergement {targetedHebergment[0].id} : {targetedHebergment[0].title}
+      <div className="hebergment-container">
+        <div className="hebergment-container__slider">
+          <img src={targetedHebergment[0].cover} alt="" />
+        </div>
+        <div className="hebergment-container__content">
+          {/* Header container */}
+          <div className="hebergment-container__header">
+            <div>
+              <h1 className="hebergment-container__header__title">
+                {targetedHebergment[0].title}
+              </h1>
+              <p className="hebergment-container__header__location">
+                {targetedHebergment[0].location}
+              </p>
+            </div>
+            <div className="hebergment-container__header__host">
+              <p className="hebergment-container__header__name">
+                {targetedHebergment[0].host.name}
+              </p>
+              <img
+                className="hebergment-container__header__picture"
+                src={targetedHebergment[0].host.picture}
+                alt=""
+              />
+            </div>
+          </div>
+          {/* Details container */}
+          <div className="hebergment-container__details">
+            {/* Tag name omponents here */}
+          </div>
+          <div className="hebergment-container__collapses">
+            {/* Collapses components */}
+          </div>
+        </div>
       </div>
     );
   }
