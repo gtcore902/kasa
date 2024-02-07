76f142d77daf06bd3d1b5c0595caa6eb41d150a4
gtcore902 <gaetan.tremois@gmail.com>
2024-02-07 18:43:27 +0100

    feat: test underline links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo_kasa.svg';
 import '../styles/Header.sass';
@@ -8,16 +8,12 @@ const Header = () => {
   // const toggleClass = () => {
   //   setIsActive(true);
   // };
-  // console.log(window.location.href.charAt(window.location.href.length - 1));
+
   return (
     <div className="navbar">
       <img className="navbar__logo" src={logo} alt="Logo Kasa" />
       <div className="navbar__box">
-        <Link
-          className="navbar__link"
-          to="/"
-          // onClick={toggleClass}
-        >
+        <Link className="navbar__link" to="/">
           {' '}
           {/* Set underline system*/}
           Accueil
